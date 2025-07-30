cc5ba81fd85b4d5164a1565693f84e074a944e16
xuwenjie <1975936508@qq.com>
2025-07-30 17:10:32 +0800

    day03 使用 Pydantic 编写请求与响应结构,提升了代码规范性

--- api/chat.py
+++ api/chat.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter
 
-from models.request_model import ChatRequest
+from models.schemas import ChatRequest, ResponseModel
 
 router = APIRouter()
 
 @router.get("/chat")
 async def chat_api(request: ChatRequest):
-    return {"response": f"你说的是:{request.query}"}
+    return ResponseModel(data={"response": f"你说的是:{request.query}"})
+
+
+@router.post("/items", response_model=ResponseModel)
+def create_item(item: ChatRequest):
+    # 假装这里存入数据库了
+    saved_item = {"id": 1, "name": item.name, "description": item.description}
+
+    return ResponseModel(data=saved_item)
--- models/request_model.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-# 解释这个类
-# 这个类定义了一个 POST 请求的参数模型，包含一个 query 字段，类型为字符串。
-class ChatRequest(BaseModel):
-    query: str = Field(..., min_length=1, max_length=200, description="用户提问内容")
