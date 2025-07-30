db43a7a0a53f4bfee1cf9eda39322e9f8bd39f5e
xuwenjie <1975936508@qq.com>
2025-07-30 16:38:18 +0800

    修改目录结构

--- app/api/chat.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from app.models.request_model import ChatRequest
-
-router = APIRouter()
-
-@router.get("/chat")
-async def chat_api(request: ChatRequest):
-    return {"response": f"你说的是:{request.query}"}
